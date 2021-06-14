bdbab29863122addce2d0e39229b31f9471db02e
Todd W Crone <twcrone@gmail.com>
2021-06-14 16:16:15 -0400

    Initial commit

---
+++ src/main/java/com/twcrone/awsv2dynamoDBsample/AwsV2DynamoDbSampleApplication.java
@@ -0,0 +1,13 @@
+package com.twcrone.awsv2dynamoDBsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsV2DynamoDbSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsV2DynamoDbSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/twcrone/awsv2dynamoDBsample/AwsV2DynamoDbSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twcrone.awsv2dynamoDBsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsV2DynamoDbSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
