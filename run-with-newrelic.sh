7de43498da1ad1cc355f3ec03be8046e401dfc00
Todd W Crone <twcrone@gmail.com>
2021-06-16 14:37:35 -0400

    Update shell command for running app

--- run-with-newrelic.sh
+++ run-with-newrelic.sh
@@ -1,3 +1,4 @@
 # NEWRELIC_ENV is directory where newrelic.yml you want to use resides (e.g. "/path/to/yml/directory")
 # NEWRELIC_JAR is full path to the newrelic.jar (e.g. "/path/to/file.jar")
-java -Dnewrelic.environment=$NEWRELIC_ENV -javaagent:$NEWRELIC_JAR -jar build/libs/*-SNAPSHOT.jar
+# with ability to attach remote debugger
+java -Dnewrelic.environment=$NEWRELIC_ENV -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address:5005 -javaagent:$NEWRELIC_JAR -jar build/libs/*-SNAPSHOT.jar
